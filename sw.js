66d05c30ebb754dcc8b15252634b84f8d8ad0ba5
Rikard Björkman <166124815+Rikard7777@users.noreply.github.com>
2025-08-12 22:50:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sudoku-v9.1";
+const CACHE = "sudoku-v9.2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", e => {
